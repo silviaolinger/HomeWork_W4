58e68f2cf43302a1f2bbe4e74b3273caa5a29620
sylvynhaarc@hotmail.com <you@example.com>
2022-10-19 13:19:18 -0500

    Css file, html file and js file create

---
+++ Assets/js/javascript.js
@@ -0,0 +1,34 @@
+const StartBtn = document.querySelector("#start-button");
+
+const questions = [{
+    question1: "Who invented the air conditioner?",
+    Answer1:"Jose",
+    Answer2:"Maria",
+    Answer3:"Willis Carrier",
+    Answer4:"Magnolia",
+    CorrectAnswer:"Willis Carrier"},
+    {
+    question2:"Who was the first president of USA?",
+    Answer1:"Jose",
+    Answer2:"Maria",
+    Answer3:"George Washington ",
+    Answer4:"Magnolia",
+    CorrectAnswer:"George Washington "},
+     {
+    question3: "What sport gets paid the most in America?",
+    Answer1:"Fottball",
+    Answer2:"Basketball",
+    Answer3:"Boxing",
+    Answer4:"Soccer",
+    CorrectAnswer:"Basketball"},
+    {
+    question4: "When is Independecy Day Celebrated in America?",
+    Answer1:"5th April",
+    Answer2:"24th January",
+    Answer3:"4th June",
+    Answer4:"4th July",
+    CorrectAnswer:"4th July"},
+
+];
+
+
